b31dc46d7ed77fe7d4886e77a11cbfcf6d42c268
Haonan Liu <messenger@lunariio.work>
2025-12-22 21:13:07 +0800

    fix: 加上少写的extern "C"

--- libs/hipnuc_sdk/include/hipnuc/canopen_parser.h
+++ libs/hipnuc_sdk/include/hipnuc/canopen_parser.h
@@ -3,6 +3,14 @@
 
 #include "hipnuc_can_common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int canopen_parse_frame(const hipnuc_can_frame_t *frame, can_sensor_data_t *data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
